1e191d865cc791e67173b853b0a08d6d3a34b53c
marcelocure <marcelocure@gmail.com>
2016-07-17 17:44:44 -0300

    Adding api call and result producing

--- node/test-v8-profiler/app.js
+++ node/test-v8-profiler/app.js
@@ -1,8 +1,16 @@
+var http = require('http-as-promised');
+var fs = require('fs');
 var profiler = require('v8-profiler');
 var snapshot = profiler.takeSnapshot('snapshot1');
 profiler.startProfiling('snapshot1');
 
-require('')
+http.get('https://api.nasa.gov/planetary/apod?api_key=WHMXIbIFDmEiTWAbnUUYw9Mvdj40xPLAZ0IhhN22')
+.then(result => {
+    console.log(JSON.stringify(result));
+    console.log(snapshot);
+    snapshot.export(function(error, result) {
+        fs.writeFileSync('snapshot1.json', result);
+        snapshot.delete();
+    });
+})
 
-profiler.stopProfiling('snapshot1');
-profiler.deleteAllSnapshots();
